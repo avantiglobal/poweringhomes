cc4acc15a64279f75e1487fd6e4f422cea801326
Adrian Gerig <adriangerig@gmail.com>
2022-01-18 22:56:59 -0500

    Dynamic Env API config file

--- public/api/config.php
+++ public/api/config.php
@@ -1,12 +1,16 @@
 <?php
+
+ if ((getenv('REMOTE_ADDR') == '127.0.0.1')){
   $db_host     = "localhost";
   $db_name     = "avanti_phapi";
   $db_user     = "root";
   $db_password = "123456";
-  // $db_host     = "localhost";
-  // $db_name     = "avanshfw_phapi";
-  // $db_user     = "avanshfw_phapi_root";
-  // $db_password = "P0w329@h0m35";
+ }else{
+  $db_host     = "localhost";
+  $db_name     = "avanshfw_phapi";
+  $db_user     = "avanshfw_phapi_root";
+  $db_password = "P0w329@h0m35";
+ }
 
   //Mail Settings
   $mail_from_name   = "Powering Homes";
