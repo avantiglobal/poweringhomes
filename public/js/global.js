7664af38e6a973f70d130f4e0f10589cd0f53715
Adrian Gerig <adriangerig@gmail.com>
2022-02-06 13:29:47 -0500

    Geolocalization commented

--- public/js/global.js
+++ public/js/global.js
@@ -39,31 +39,31 @@ $(document).ready(function () {
         //     maximumAge: 0
         // };
 
-        function showLocation(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            console.log("Latitude : " + latitude + " Longitude: " + longitude);
-        }
+        // function showLocation(position) {
+        //     var latitude = position.coords.latitude;
+        //     var longitude = position.coords.longitude;
+        //     console.log("Latitude : " + latitude + " Longitude: " + longitude);
+        // }
 
-        function errorHandler(err) {
-            if (err.code == 1) {
-                console.log("Error: Geo localization Access is denied!");
-            } else if (err.code == 2) {
-                console.log("Error: Position is unavailable!");
-            }
-        }
+        // function errorHandler(err) {
+        //     if (err.code == 1) {
+        //         console.log("Error: Geo localization Access is denied!");
+        //     } else if (err.code == 2) {
+        //         console.log("Error: Position is unavailable!");
+        //     }
+        // }
 
-        function getLocation() {
+        // function getLocation() {
 
-            if (navigator.geolocation) {
+        //     if (navigator.geolocation) {
 
-                // timeout at 60000 milliseconds (60 seconds)
-                var options = { timeout: 60000 };
-                navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
-            } else {
-                console.log("Sorry, browser does not support geolocation!");
-            }
-        }
+        //         // timeout at 60000 milliseconds (60 seconds)
+        //         var options = { timeout: 60000 };
+        //         navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+        //     } else {
+        //         console.log("Sorry, browser does not support geolocation!");
+        //     }
+        // }
 
         getLocation();
         // var url = new URL('https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/')
